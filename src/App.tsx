233d26da19719068d9556c26e942cf90eb60def1
R0EYK <roeyk70@gmail.com>
2025-10-18 00:10:32 +0300

    Removed failing on purpose

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
-throw new Error('Testing build fail');
-
 export default function App() {
   return (
     <MantineProvider theme={theme}>
